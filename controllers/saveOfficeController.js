30bb0bfae43a117989b6efcb9f6e7e6a8e3d8dcf
Mark Allen <markallen.webdeveloper@gmail.com>
2019-11-28 12:53:04 +0000

    Testing behaviour with CORS problem

--- controllers/saveOfficeController.js
+++ controllers/saveOfficeController.js
@@ -4,9 +4,10 @@ const Offices = require("../models/Offices.js");
 
 module.exports = async (req, res) => {
 
+       /*
    await Offices.create({
     
-    /*
+
      office: req.body.office,
      building: req.body.building,
      number: req.body.number,
@@ -14,9 +15,9 @@ module.exports = async (req, res) => {
      town: req.body.town,
      postcode: req.body.postcode,
      adminLock: req.body.adminLock
-     */
    });
+     */
    //res.json(data);
-   res.send("Done!" + req.body.office);
+   res.send("Done! " + req.body.office);
 
 };
