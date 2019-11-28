b5de545e88260f17c12fb3717aaf7d8ee82cbb3d
Mark Allen <markallen.webdeveloper@gmail.com>
2019-11-28 13:40:33 +0000

    Testing behaviour with CORS problem

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // CORS support
-app.use(cors());
+//app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // controllers
 const getOfficesController = require('./controllers/getOfficesController')
