6df4d07462cb83cb8e692408a9d7ca8f3c52f14d
Mark Allen <markallen.webdeveloper@gmail.com>
2019-11-29 14:39:32 +0000

    Modified backend to pass success/failure info on Office create to frontend

--- controllers/saveOfficeController.js
+++ controllers/saveOfficeController.js
@@ -3,15 +3,28 @@
 const Offices = require("../models/Offices.js");
 
 module.exports = async (req, res) => {
-   await Offices.create({  
-     office: req.body.office,
-     building: req.body.building,
-     number: req.body.number,
-     street: req.body.street,
-     town: req.body.town,
-     postcode: req.body.postcode,
-     adminLock: req.body.adminLock
-   });
-
-  res.send("bing!");
+  await Offices.create(
+    {
+      office: req.body.office,
+      building: req.body.building,
+      number: req.body.number,
+      street: req.body.street,
+      town: req.body.town,
+      postcode: req.body.postcode,
+      adminLock: req.body.adminLock
+    },
+    function(err, document) {
+      if (err) {
+        res.json({
+          status: "Fail",
+          message: "Not allowed - duplicate record!"
+        });
+      } else {
+        res.json({
+          status: "Success",
+          message: "Recorded added to system!"
+        });
+      }
+    }
+  );
 };
