bd362ff48bb61e9171240664e02305859c85df10
Mark Allen <markallen.webdeveloper@gmail.com>
2019-12-09 15:35:43 +0000

    Working on delete

--- controllers/deleteOfficeController.js
+++ controllers/deleteOfficeController.js
@@ -3,7 +3,9 @@
 const Offices = require('../models/Offices.js')
 
 module.exports = async (req, res) => {
-  const office = await Offices.deleteOne({ _id: req.params.id })
+  const office = await Offices.deleteOne({ office: req.body.office })
+  console.log("Delete request received")
+  console.log(req.body.office);
   res.json({
     status: "Success",
     message: "Record deleted"
